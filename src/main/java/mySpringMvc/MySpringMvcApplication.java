49631b88673dc033c63b4390b12943905a3332ba
sidney zhang <sidneyzsc@gmail.com>
2016-12-22 16:56:25 +0800

    fisrt commit

---
+++ src/main/java/mySpringMvc/MySpringMvcApplication.java
@@ -0,0 +1,12 @@
+package mySpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/mySpringMvc/MySpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package mySpringMvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MySpringMvcApplication.class)
+public class MySpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
